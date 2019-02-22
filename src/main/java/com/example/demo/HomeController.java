6281c40d7abdd0f392f4ad3e71bbe94f3fd9fb18
sw333zy <sw333zy@gmail.com>
2019-02-22 14:18:24 -0500

    added blog posts

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -13,11 +13,68 @@ public class HomeController {
 
     @RequestMapping("/bootcamp")
     public String bootcamp(){
-        return "authors";
+        return "bootcamp";
     }
 
     @RequestMapping("/authors")
     public String authors(){
-        return "bootcamp";
+        return "author";
+    }
+    @RequestMapping("/blogposts")
+    public String blogposts(){
+        return "blogposts";
     }
 }
+
+
+
+//    @RequestMapping("/jan2819")
+//    public String jan2819(){
+//        return "jan2819";
+//    }
+//
+//    @RequestMapping("/jan3019")
+//    public String jan3019(){
+//        return "jan3019";
+//    }
+//
+//    @RequestMapping("/feb0419")
+//    public String authors(){
+//        return "author";
+//    }
+//
+//    @RequestMapping("/feb0619")
+//    public String authors(){
+//        return "author";
+//    }
+//
+//    @RequestMapping("/feb1019")
+//    public String authors(){
+//        return "author";
+//    }
+//
+//    @RequestMapping("/feb1319")
+//    public String authors(){
+//        return "author";
+//    }
+//
+//    @RequestMapping("/feb1519")
+//    public String authors(){
+//        return "author";
+//    }
+//
+//    @RequestMapping("/feb1719")
+//    public String authors(){
+//        return "author";
+//    }
+//
+//    @RequestMapping("/feb1919")
+//    public String authors(){
+//        return "author";
+//    }
+//
+//    @RequestMapping("/feb2019")
+//    public String feb2019(){
+//        return "feb2019";
+//    }
+//}
